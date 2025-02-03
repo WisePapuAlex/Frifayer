e54820cd449666d34e4d036175654a0ce0829d86
Génesis Alexandra Acuña Jirón <genesisacunajiron@gmail.com>
2025-02-03 00:06:27 -0600

    eso

--- Frifayer/Form1.Designer.cs
+++ Frifayer/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pnl_Principal = new Panel();
-            pictureBox3 = new PictureBox();
             pnl_central = new Panel();
             pnl_3 = new Panel();
             label2 = new Label();
@@ -44,19 +43,37 @@
             lbl_Olvidarcontraseña = new Label();
             pnl_2 = new Panel();
             bt_interfaz = new Button();
+            pictureBox3 = new PictureBox();
+            panel1 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox4 = new PictureBox();
+            label5 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
+            label6 = new Label();
+            label8 = new Label();
+            comboBox2 = new ComboBox();
             pnl_Principal.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             pnl_central.SuspendLayout();
             pnl_3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             pnl_2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // pnl_Principal
             // 
             pnl_Principal.BackgroundImage = Properties.Resources.pexels_codioful_6985001;
             pnl_Principal.BackgroundImageLayout = ImageLayout.Stretch;
+            pnl_Principal.Controls.Add(panel1);
             pnl_Principal.Controls.Add(pnl_central);
             pnl_Principal.Controls.Add(pictureBox3);
             pnl_Principal.Dock = DockStyle.Fill;
@@ -65,17 +82,6 @@
             pnl_Principal.Size = new Size(800, 450);
             pnl_Principal.TabIndex = 0;
             pnl_Principal.Paint += pnl_Principal_Paint;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(775, 0);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(26, 23);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 30;
-            pictureBox3.TabStop = false;
-            pictureBox3.Click += pictureBox3_Click;
             // 
             // pnl_central
             // 
@@ -99,7 +105,6 @@
             pnl_3.Controls.Add(tb_Contraseña);
             pnl_3.Controls.Add(tb_Correo);
             pnl_3.Controls.Add(lbl_Olvidarcontraseña);
-            pnl_3.Dock = DockStyle.Right;
             pnl_3.Location = new Point(283, 0);
             pnl_3.Name = "pnl_3";
             pnl_3.Size = new Size(425, 370);
@@ -149,11 +154,11 @@
             // lbl_Iniciodesesion
             // 
             lbl_Iniciodesesion.AutoSize = true;
-            lbl_Iniciodesesion.Font = new Font("Century", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_Iniciodesesion.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lbl_Iniciodesesion.ForeColor = Color.DimGray;
             lbl_Iniciodesesion.Location = new Point(139, 85);
             lbl_Iniciodesesion.Name = "lbl_Iniciodesesion";
-            lbl_Iniciodesesion.Size = new Size(163, 23);
+            lbl_Iniciodesesion.Size = new Size(157, 24);
             lbl_Iniciodesesion.TabIndex = 25;
             lbl_Iniciodesesion.Text = "Inicio de sesión";
             // 
@@ -161,7 +166,7 @@
             // 
             btn_Iniciarsesion.BackColor = Color.Pink;
             btn_Iniciarsesion.FlatStyle = FlatStyle.Flat;
-            btn_Iniciarsesion.Font = new Font("Century Gothic", 9.75F);
+            btn_Iniciarsesion.Font = new Font("Microsoft Sans Serif", 9.75F);
             btn_Iniciarsesion.ForeColor = Color.Black;
             btn_Iniciarsesion.Location = new Point(234, 273);
             btn_Iniciarsesion.Name = "btn_Iniciarsesion";
@@ -173,11 +178,11 @@
             // tb_Contraseña
             // 
             tb_Contraseña.BorderStyle = BorderStyle.None;
-            tb_Contraseña.Font = new Font("Century Gothic", 9.75F);
+            tb_Contraseña.Font = new Font("Microsoft Sans Serif", 9.75F);
             tb_Contraseña.ForeColor = Color.Gray;
             tb_Contraseña.Location = new Point(55, 200);
             tb_Contraseña.Name = "tb_Contraseña";
-            tb_Contraseña.Size = new Size(350, 16);
+            tb_Contraseña.Size = new Size(350, 15);
             tb_Contraseña.TabIndex = 23;
             tb_Contraseña.Text = "Contraseña";
             tb_Contraseña.TextAlign = HorizontalAlignment.Center;
@@ -185,11 +190,11 @@
             // tb_Correo
             // 
             tb_Correo.BorderStyle = BorderStyle.None;
-            tb_Correo.Font = new Font("Century Gothic", 9.75F);
+            tb_Correo.Font = new Font("Microsoft Sans Serif", 9.75F);
             tb_Correo.ForeColor = Color.Gray;
             tb_Correo.Location = new Point(55, 149);
             tb_Correo.Name = "tb_Correo";
-            tb_Correo.Size = new Size(350, 16);
+            tb_Correo.Size = new Size(350, 15);
             tb_Correo.TabIndex = 22;
             tb_Correo.Text = "Correo";
             tb_Correo.TextAlign = HorizontalAlignment.Center;
@@ -197,17 +202,17 @@
             // lbl_Olvidarcontraseña
             // 
             lbl_Olvidarcontraseña.AutoSize = true;
-            lbl_Olvidarcontraseña.Font = new Font("Century Gothic", 9.75F);
+            lbl_Olvidarcontraseña.Font = new Font("Microsoft Sans Serif", 9.75F);
             lbl_Olvidarcontraseña.ForeColor = Color.Black;
             lbl_Olvidarcontraseña.Location = new Point(16, 279);
             lbl_Olvidarcontraseña.Name = "lbl_Olvidarcontraseña";
-            lbl_Olvidarcontraseña.Size = new Size(179, 17);
+            lbl_Olvidarcontraseña.Size = new Size(161, 16);
             lbl_Olvidarcontraseña.TabIndex = 21;
             lbl_Olvidarcontraseña.Text = "¿Olvidaste tu contraseña?";
             // 
             // pnl_2
             // 
-            pnl_2.BackColor = Color.Pink;
+            pnl_2.BackColor = Color.PaleGreen;
             pnl_2.Controls.Add(bt_interfaz);
             pnl_2.Dock = DockStyle.Left;
             pnl_2.Location = new Point(0, 0);
@@ -219,13 +224,184 @@
             // 
             bt_interfaz.BackColor = Color.Pink;
             bt_interfaz.FlatStyle = FlatStyle.Flat;
-            bt_interfaz.Font = new Font("Century Gothic", 9.75F);
+            bt_interfaz.Font = new Font("Microsoft Sans Serif", 9.75F);
             bt_interfaz.Location = new Point(206, 109);
             bt_interfaz.Name = "bt_interfaz";
             bt_interfaz.Size = new Size(74, 28);
             bt_interfaz.TabIndex = 11;
             bt_interfaz.Text = "Login";
             bt_interfaz.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(775, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(26, 23);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 30;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(textBox2);
+            panel1.Location = new Point(34, 37);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(425, 370);
+            panel1.TabIndex = 30;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Location = new Point(16, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(392, 15);
+            label3.TabIndex = 29;
+            label3.Text = "_____________________________________________________________________________";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Location = new Point(16, 106);
+            label4.Name = "label4";
+            label4.Size = new Size(392, 15);
+            label4.TabIndex = 28;
+            label4.Text = "_____________________________________________________________________________";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(23, 89);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(26, 23);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 27;
+            pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.DimGray;
+            label5.Location = new Point(150, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(115, 24);
+            label5.TabIndex = 25;
+            label5.Text = "Registrarse";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Pink;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft Sans Serif", 9.75F);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(121, 325);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 28);
+            button1.TabIndex = 24;
+            button1.Text = "Registrarse";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Microsoft Sans Serif", 9.75F);
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(43, 196);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(350, 15);
+            textBox1.TabIndex = 23;
+            textBox1.Text = "Contraseña";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Microsoft Sans Serif", 9.75F);
+            textBox2.ForeColor = Color.Gray;
+            textBox2.Location = new Point(43, 88);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(350, 15);
+            textBox2.TabIndex = 22;
+            textBox2.Text = "Nombre completo";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Microsoft Sans Serif", 9.75F);
+            textBox3.ForeColor = Color.Gray;
+            textBox3.Location = new Point(43, 142);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(350, 15);
+            textBox3.TabIndex = 30;
+            textBox3.Text = "Usuario";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Location = new Point(23, 160);
+            label7.Name = "label7";
+            label7.Size = new Size(392, 15);
+            label7.TabIndex = 31;
+            label7.Text = "_____________________________________________________________________________";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(33, 276);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 32;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.WhiteSmoke;
+            label6.Font = new Font("Microsoft Sans Serif", 9.75F);
+            label6.ForeColor = Color.Gray;
+            label6.Location = new Point(74, 243);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 16);
+            label6.TabIndex = 33;
+            label6.Text = "Rol";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft Sans Serif", 9.75F);
+            label8.ForeColor = Color.Gray;
+            label8.Location = new Point(237, 243);
+            label8.Name = "label8";
+            label8.Size = new Size(112, 16);
+            label8.TabIndex = 34;
+            label8.Text = "Estado de cuenta";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(237, 276);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 35;
             // 
             // Form1
             // 
@@ -243,13 +419,16 @@
             Text = "Citas";
             Load += Form1_Load;
             pnl_Principal.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             pnl_central.ResumeLayout(false);
             pnl_3.ResumeLayout(false);
             pnl_3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             pnl_2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
@@ -270,5 +449,19 @@
         private Label lbl_Olvidarcontraseña;
         private Panel pnl_2;
         private Button bt_interfaz;
+        private Panel panel1;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox4;
+        private Label label5;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label8;
+        private Label label6;
+        private ComboBox comboBox1;
+        private Label label7;
+        private TextBox textBox3;
+        private ComboBox comboBox2;
     }
 }
