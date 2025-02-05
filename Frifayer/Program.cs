bca661a0bc70a9c356d2b0cd5522f8f0cf1ca227
Darwin DarKinG <darwinjiron56@gmail.com>
2025-02-05 00:05:06 -0600

    barra

--- Frifayer/Program.cs
+++ Frifayer/Program.cs
@@ -1,3 +1,5 @@
+using Frifayer.forms_paneles;
+
 namespace Frifayer
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Frifayer
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new menuprincipal());
+            Application.Run(new barra_lateral());
         }
     }
 }
---
+++ Frifayer/forms_paneles/barra_lateral.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frifayer.forms_paneles
+{
+    public partial class barra_lateral : Form
+    {
+        public barra_lateral()
+        {
+            InitializeComponent();
+        }
+    }
+}
